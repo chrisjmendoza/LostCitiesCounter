c7c55ce8945f2bcff9dc0a38c62d37abf7d78f59
Chris Mendoza <chrisjmendoza@gmail.com>
2016-04-23 11:36:11 -0700

    Initial Files

---
+++ app/src/main/java/com/ajochcas/lostcitiescounter/ScoreDisplay.java
@@ -0,0 +1,13 @@
+package com.ajochcas.lostcitiescounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScoreDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score_display);
+    }
+}
