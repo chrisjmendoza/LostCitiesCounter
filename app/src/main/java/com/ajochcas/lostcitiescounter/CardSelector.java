3e69241994e7802c48a53b37aa763ba6cdc90438
Chris Mendoza <chrisjmendoza@gmail.com>
2016-04-28 10:47:03 -0700

    Button Changes on card selection screen

--- app/src/main/java/com/ajochcas/lostcitiescounter/CardSelector.java
+++ app/src/main/java/com/ajochcas/lostcitiescounter/CardSelector.java
@@ -1,5 +1,6 @@
 package com.ajochcas.lostcitiescounter;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class CardSelector extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -40,6 +42,15 @@ public class CardSelector extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        Button nextButton = (Button) findViewById(R.id.nextButton);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CardSelector.this, ScoreDisplay.class);
+            }
+
+        });
     }
 
     @Override
