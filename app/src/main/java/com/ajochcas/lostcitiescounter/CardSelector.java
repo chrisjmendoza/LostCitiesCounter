27c4d558bffd0887fc550ca39cda101083ef52f4
Chris Mendoza <chrisjmendoza@gmail.com>
2016-05-04 19:42:39 -0700

    NEED TO REMOVE TOOLBAR FIGURE IT OUT

--- app/src/main/java/com/ajochcas/lostcitiescounter/CardSelector.java
+++ app/src/main/java/com/ajochcas/lostcitiescounter/CardSelector.java
@@ -2,16 +2,12 @@ package com.ajochcas.lostcitiescounter;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -25,10 +21,6 @@ public class CardSelector extends AppCompatActivity
     Activity activity;
 
     Button[] buttonArray = new Button[12];
-
-    private MediaPlayer volcano;
-    private boolean allowMusic;
-    private boolean isPlaying = true;
 
     // Define the 5 card sets
     private ExpeditionCardSet cardSet = new ExpeditionCardSet();
@@ -58,15 +50,6 @@ public class CardSelector extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card_selector);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        assert drawer != null;
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
 
         activity = this;
 
@@ -86,10 +69,6 @@ public class CardSelector extends AppCompatActivity
 
         // Change the background based on which expedition is active
         ActiveExpedition(activeExpedition);
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        assert navigationView != null;
-        navigationView.setNavigationItemSelectedListener(this);
 
         buttonArray[0].setOnClickListener(new View.OnClickListener() {
             @Override
@@ -487,21 +466,6 @@ public class CardSelector extends AppCompatActivity
         buttonClicks();
     }
 
-    /**
-     * Pauses or resumes the music, or doesn't play at all if the preference is turned off
-     */
-    protected void musicPause() {
-        if (allowMusic) {
-            if (isPlaying) {
-                volcano.pause();
-                isPlaying = false;
-            } else {
-                volcano.start();
-                isPlaying = true;
-            }
-        }
-    }
-
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -511,28 +475,6 @@ public class CardSelector extends AppCompatActivity
         } else {
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.card_selector, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
@@ -558,9 +500,6 @@ public class CardSelector extends AppCompatActivity
 
         } else if (id == R.id.nav_share) {
 
-//        } else if (id == R.id.scoreScreen) {
-//
-//        }
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
