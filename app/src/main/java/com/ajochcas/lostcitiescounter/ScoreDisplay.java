a1de7495eac1cfe504f560f7acf601e526ead79c
changsai39 <changsai39@gmail.com>
2016-05-04 18:55:09 -0700

    Update ScoreDisplay.java

--- app/src/main/java/com/ajochcas/lostcitiescounter/ScoreDisplay.java
+++ app/src/main/java/com/ajochcas/lostcitiescounter/ScoreDisplay.java
@@ -1,98 +1,8 @@
-/*
 package com.ajochcas.lostcitiescounter;
 
+import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Scanner;
-
-public class ScoreDisplay extends AppCompatActivity {
-
-    private int thisScore;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_score_display);
-
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(CardSelector.SCORE_OUTPUT);
-
-        TextView textView = (TextView) findViewById(R.id.scoreView);
-        assert textView != null;
-        textView.setTextSize(40);
-        textView.setText(message);
-
-        Button homeButton = (Button) findViewById(R.id.homeButton);
-        assert homeButton != null;
-        homeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(ScoreDisplay.this, Landing.class));
-            }
-        });
-
-        //save to file under here
-
-        thisScore = Integer.parseInt(message);
-
-        Button save = (Button) findViewById(R.id.save);
-
-        assert save != null;
-        save.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Scanner in = null;
-
-                File file = new File("res/raw/score.txt");
-
-                try {
-                    in = new Scanner(file);
-                } catch (FileNotFoundException ignored) {
-
-                }
-
-                int scores[] = new int[]{-401, -401, -401, -401, -401, -401, -401, -401, -401, -401};
-                int index = 0;
-
-                assert in != null;
-                while (in.hasNext()) {
-                    scores[index] = in.nextInt();
-                    index++;
-                }
-
-                File overwrite = new File("res/raw/score.txt");
-
-                try {
-                    FileWriter write = new FileWriter(overwrite);
-                    write.write("" + thisScore);
-
-                    for (int i = 0; i < 9; i++) {
-                        write.write(" " + scores[i]);
-                    }
-
-                } catch (IOException ignored) {
-
-                }
-            }
-        });
-    }
-}
-
-*/
-
-package com.ajochcas.lostcitiescounter;
-
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -146,30 +56,19 @@ public class ScoreDisplay extends AppCompatActivity {
 
                 save.setClickable(false);
 
-                Scanner in = new Scanner(getApplicationContext().getResources().openRawResource(R.raw.scores));
+                SharedPreferences sharedPreferences;
 
-                File file = getFilesDir();
+                sharedPreferences = getSharedPreferences("scores", Context.MODE_PRIVATE);
 
-                scores = new int[]{-401, -401, -401, -401, -401, -401, -401, -401, -401, -401};
-                int index = 0;
+                String temp = sharedPreferences.getString("scores", null);
 
-                while (in.hasNext()) {
-                    scores[index] = in.nextInt();
-                    index++;
-                }
+                SharedPreferences.Editor edit = sharedPreferences.edit();
 
-                try {
-                    PrintWriter write = new PrintWriter(new File(file, "scores.txt"));
-                    write.print("" + thisScore);
+                edit.clear();
 
-                    for(int i = 0; i < 9; i++) {
-                        write.write(" " + scores[i]);
-                    }
-                    write.close();
-                } catch (FileNotFoundException e) {
-                    Boolean a = null;
-                    while(a.booleanValue()){;}
-                }
+                edit.putString("scores", "" + thisScore + temp);
+
+                edit.commit();
 
 
                 /*
@@ -203,4 +102,3 @@ public class ScoreDisplay extends AppCompatActivity {
         });
     }
 }
-
